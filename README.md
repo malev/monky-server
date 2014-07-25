41bee6037960ea1511d15a52b407fb2a94e6d3b3
malev <marcosvanetta@gmail.com>
2014-07-24 20:47:33 -0400

    Fabric files for analice.me

---
+++ README.md
@@ -0,0 +1,22 @@
+# Fabric recipe for Analice.me
+
+> pip install fabric
+> fab -H root@YOURSERVERIP first_steps
+
+And you have a user named **deploy**, elasticsearch and some basic packages.
+
+> fab -H deploy@YOURSERVERIP ruby
+
+And you have **Ruby 2.1.2** installed with **rbenv**.
+
+> fab -H deploy@YOURSERVERIP freeling
+
+And FREELING!!!!!
+
+## TODO
+
+* Put everything in a single command.
+* Add security configurations for SSH.
+* Add recipes for other stuff.
+
+http://code.tutsplus.com/tutorials/setting-up-a-rails-server-and-deploying-with-capistrano-on-fedora-from-scratch--net-10443
---
+++ fabfile.py
@@ -0,0 +1,91 @@
+import os
+from fabric.api import run, settings
+
+
+def install(dependencies):
+    run('sudo apt-get install -y ' + ' '.join(dependencies))
+
+def run_rbenv(cad):
+    run('PATH="$HOME/.rbenv/bin:$PATH" ' + cad)
+
+def freeling():
+    dependencies = [
+        'libxml2-dev', 'libxslt1-dev', 'libicu-dev', 'libboost-all-dev',
+        'zlib1g-dev', 'graphicsmagick', 'poppler-utils', 'poppler-data',
+        'ghostscript', 'pdftk', 'libreoffice', 'libboost-thread-dev'
+    ]
+    install(dependencies)
+    run('wget http://devel.cpl.upc.edu/freeling/downloads/32')
+    run('tar xvzf freeling.tar.gz')
+    run('cd freeling-3.1')
+    run('./configure')
+    run('make')
+    run('sudo make install')
+
+
+def nodejs():
+    run('git clone https://github.com/creationix/nvm.git ~/.nvm')
+    run("echo 'source ~/.nvm/nvm.sh' >> .bashrc")
+    run('source ~/.nvm/nvm.sh')
+    run('nvm install 0.10')
+    run('nvm alias default 0.10')
+
+
+def ruby():
+    install(['build-essential', 'libssl-dev'])
+    run('git clone https://github.com/sstephenson/rbenv.git ~/.rbenv')
+    run("echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc")
+    run("echo 'eval \"$(rbenv init -)\"' >> ~/.bashrc")
+    run('git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build')
+
+    run_rbenv('rbenv install 2.1.2')
+    run_rbenv('rbenv global 2.1.2')
+    with settings(warn_only=True):
+        run('export PATH=$HOME/.rbenv/bin:$PATH')
+        run('eval "$(~/.rbenv/bin/rbenv init -)"')
+    run('~/.rbenv/shims/gem install bundler')
+    run('~/.rbenv/bin/rbenv rehash')
+
+
+def elasticsearch():
+    run('wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.0.deb')
+    run('dpkg -i elasticsearch-1.3.0.deb')
+    run('echo "script.disable_dynamic: false" /etc/elasticsearch/elasticsearch.yml')
+    run('echo "network.bind_host: 127.0.0.1" /etc/elasticsearch/elasticsearch.yml')
+
+
+def remove_user(username="deploy"):
+    run('sudo userdel -r ' + username)
+
+
+def create_user(username="deploy"):
+    key = ""
+    with open(os.getenv("HOME") + '/.ssh/id_rsa.pub', 'r') as content_file:
+        key = content_file.read()
+
+    run('useradd ' + username +' -m')
+    run('mkdir /home/' + username + '/.ssh')
+    run('passwd ' + username)
+    run('echo "' + key + '" > /home/' + username + '/.ssh/authorized_keys')
+    run('chmod 700 /home/' + username +'/.ssh')
+    run('chown ' + username +':' + username +' /home/' + username+ ' -R')
+    run('adduser ' + username + ' sudo')
+    run('usermod -s /bin/bash ' + username)
+
+
+def basic_packages():
+    dependencies = ['build-essential', 'git-core', 'mongodb', 'mongodb-server', 'redis-server']
+    install(dependencies)
+
+
+def closing_ssh():
+    pass
+
+
+def first_steps(user="deploy"):
+    run('apt-get update -y')
+    run('apt-get upgrade -y')
+    basic_packages()
+    elasticsearch()
+    create_user(user)
+    closing_ssh()
