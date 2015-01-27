a9a0a08f2be3eebbb78cb3fb577862b1ae41170a
Marcos Vanetta <marcosvanetta@gmail.com>
2015-01-26 21:02:22 -0600

    logs available on production

--- aphrodite/fabfile.py
+++ aphrodite/fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import *
 
-env.hosts = "example.org"
-env.user = "user"
+env.hosts = "analiceme.cloudapp.net"
+env.user = "deploy"
 env.key_filename = "~/.ssh/myPrivateKey.key"
 env.port = "22"
 
@@ -11,14 +11,13 @@ def install(packages):
     sudo('apt-get install -y ' + ' '.join(packages))
     put('nginx.conf', '/etc/nginx/sites-enabled/default')
     sudo('service nginx restart')
-    sudo('docker login')
 
 def deploy():
     put('config.dat', '~/')
-    sudo('docker pull malev/aphrodite')
+    # sudo('docker pull malev/aphrodite:latest')
     sudo('docker rm -f aphrodite-1')
-    sudo('docker run -d -p 3001:8080 --env-file=config.dat --name=aphrodite-1 malev/aphrodite')
+    sudo('docker run -d -p 3001:8080 --env-file=config.dat --name=aphrodite-1 -v ~/log:/app/aphrodite/log malev/aphrodite')
     sudo('docker rm -f aphrodite-2')
-    sudo('docker run -d -p 3002:8080 --env-file=config.dat --name=aphrodite-2 malev/aphrodite')
+    sudo('docker run -d -p 3002:8080 --env-file=config.dat --name=aphrodite-2 -v ~/log:/app/aphrodite/log malev/aphrodite')
     sudo('docker rm -f aphrodite-3')
-    sudo('docker run -d -p 3003:8080 --env-file=config.dat --name=aphrodite-3 malev/aphrodite')
+    sudo('docker run -d -p 3003:8080 --env-file=config.dat --name=aphrodite-3 -v ~/log:/app/aphrodite/log malev/aphrodite')
